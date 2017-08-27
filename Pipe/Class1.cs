4c345fbf397249019e98378842cd3f4e65a70a1b
suzdalnitski <suzdalnitski@gmail.com>
2017-08-27 12:28:18 -0600

    Initial commit.

---
+++ Pipe/Class1.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+using static Tests.PipeBase;
+
+//using static Tests.PipeBase;
+
+namespace Tests
+{
+	public static class DelegateUtility
+	{
+		public static T Cast<T>(Delegate source) where T : class
+		{
+			return Cast(source, typeof(T)) as T;
+		}
+
+		public static Delegate Cast(Delegate source, Type type)
+		{
+			if (source == null)
+				return null;
+			Delegate[] delegates = source.GetInvocationList();
+			if (delegates.Length == 1)
+				return Delegate.CreateDelegate(type,
+					delegates[0].Target, delegates[0].Method);
+			Delegate[] delegatesDest = new Delegate[delegates.Length];
+			for (int nDelegate = 0; nDelegate < delegates.Length; nDelegate++)
+				delegatesDest[nDelegate] = Delegate.CreateDelegate(type,
+					delegates[nDelegate].Target, delegates[nDelegate].Method);
+			return Delegate.Combine(delegatesDest);
+		}
+	}
+
+	public static class TypeExtensions {
+		/// <summary>
+		/// Compares object with a given type.
+		/// </summary>
+		public static bool Is<T>( this Type type ) {
+			return type == typeof(T);
+		}
+
+		/// <summary>
+		/// Casts object to a given type.
+		/// </summary>
+		public static T To<T>( this object obj ) {
+			try {
+				return (T) obj;
+			} catch (Exception) {
+				throw new InvalidOperationException($"Unable to cast type [{obj.GetType()}] to {typeof(T)}.");
+			}
+		}
+	}
+	public static class FuncBaseExtensions {
+		/*public static PipeBase F( this F func ) => new PipeBase(func);
+		public static PipeBase A( this A act ) => new PipeBase(act);
+		public static PipeBase F( this object obj ) => new PipeBase(i => obj);*/
+
+		//public static F F<TIn, TOut>(Func<TIn, TOut> func ) => i => func(i);
+
+		//public static F ToF(Delegate func) => date => func(date());
+		//public static F DynFunc(this Func<dynamic, dynamic> func) => date => func(date);
+
+		//public static Func<dynamic, dynamic> ToDynamicFunc() => 
+
+	}
+
+	[TestFixture]
+	public class Tests {
+		private F Add( int b ) => a => (int) a + b;
+		private F Mult( int b ) => a => (int) a * b;
+		private F Mult( float b ) => a => (float) a * b;
+
+		private F AddDays( int days ) => date => date.To<DateTime>().AddDays(days);
+
+		private string GetLongDate( DateTime date ) => date.ToLongDateString();
+
+		private F GetLongDateF() => i => GetLongDate(i.To<DateTime>());
+
+		private F GetShortDate() => date => date.To<DateTime>().ToShortDateString();
+
+		private A WriteToConsole() => Console.WriteLine;
+
+		[Test]
+		public void Test() {
+			Pipe(P | DateTime.Now | AddDays(-1) | GetShortDate() | (A)Console.WriteLine);
+
+			var yesterday = Pipe( P | DateTime.Now | AddDays(-1) | GetShortDate() | (A)Console.WriteLine) );
+			yesterday.Do();
+			//Console.WriteLine(yesterday.Get());
+
+			var seq = P | DateTime.Now | AddDays(10) | GetLongDateF() | WriteToConsole();
+			seq.Do();
+		}
+	}
+}
---
+++ Pipe/DynamicPipe.cs
@@ -0,0 +1,251 @@
+﻿using JetBrains.Annotations;
+using System;
+
+namespace Tests
+{
+	//public delegate dynamic F(dynamic obj);
+	//public delegate void A(dynamic obj);
+	
+	public struct PipeDynamic
+	{
+		private bool IsFunc => _func != null;
+		private bool IsAct  => _act != null;
+		private bool IsInitialized => IsFunc || IsAct;
+
+		private readonly F _func;
+
+		private F Func {
+			get {
+				if (!IsFunc) throw new InvalidOperationException($"Pipe is not a function.");
+
+				return _func;
+			}
+		}
+
+		private readonly A _act;
+
+		private A Act {
+			get {
+				if (!IsAct) throw new InvalidOperationException($"Pipe is not an action.");
+
+				return _act;
+			}
+		}
+
+		public dynamic Get()
+		{
+			if (!IsInitialized) throw new InvalidOperationException("Uninitialized pipe.");
+			if (!IsFunc) throw new InvalidOperationException("Calling wrong pipe method. Should Do() instead.");
+
+			return Func(null);
+		}
+
+		public void Do()
+		{
+			if (!IsInitialized) throw new InvalidOperationException("Uninitialized pipe.");
+			if (!IsAct) throw new InvalidOperationException("Calling wrong pipe method. Should Get() instead.");
+
+			Act(null);
+		}
+
+		/// <summary>
+		/// Returns a new pipe
+		/// </summary>
+		public static PipeDynamic Pipe => new PipeDynamic();
+
+		private PipeDynamic([NotNull] F func)
+		{
+			if (func == null) throw new ArgumentNullException(nameof(func));
+			_func = func;
+			_act = null;
+		}
+
+		private PipeDynamic([NotNull] A act)
+		{
+			if (act == null) throw new ArgumentNullException(nameof(act));
+			_act = act;
+			_func = null;
+		}
+
+		/// <summary>
+		/// Forward pipe operator
+		/// </summary>
+		public static PipeDynamic operator |(PipeDynamic lhs, PipeDynamic rhs)
+		{
+			if (rhs.IsFunc) {
+				Func<object, object> combined = i => rhs.Func(lhs.Func(i));
+				return new PipeDynamic(i => combined(i));
+			} else {
+				Action<object> combined = i => rhs.Act(lhs.Func(i));
+				return new PipeDynamic(i => combined(i));
+			}
+		}
+
+		/// <summary>
+		/// Forward pipe operator
+		/// </summary>
+		public static PipeDynamic operator |(PipeDynamic lhs, F rhs)
+		{
+			Func<object, object> combined = i => rhs(lhs.Func(i));
+			return new PipeDynamic( (F) (i => combined(i)) );
+		}
+
+		/// <summary>
+		/// Forward pipe operator
+		/// </summary>
+		public static PipeDynamic operator |(PipeDynamic lhs, A rhs)
+		{
+			Action<object> combined = i => rhs(lhs.Func(i));
+			return new PipeDynamic( (A) (i => combined(i)) );
+		}
+		
+		/// <summary>
+		/// Forward pipe operator. Initializes a pipe with object.
+		/// </summary>
+		public static PipeDynamic operator |(PipeDynamic lhs, object rhs)
+		{
+			if (lhs.IsInitialized)
+				throw new InvalidOperationException($"Pipe is already initialized, cannot initialize with object of type {rhs.GetType()}");
+
+			return new PipeDynamic(i => rhs);
+		}
+
+		public static implicit operator PipeDynamic(F func)
+		{
+			return new PipeDynamic(func);
+		}
+
+		public static implicit operator PipeDynamic(A act)
+		{
+			return new PipeDynamic(act);
+		}
+	}
+	/*
+	public sealed class DynamicPipe : DynamicObject {
+		private readonly Dictionary<string, object> _properties = new Dictionary<string, object>();
+
+		public override bool TryGetMember(GetMemberBinder binder, out object result)
+		{
+			return _properties.TryGetValue(binder.Name, out result);
+		}
+
+		public override bool TrySetMember(SetMemberBinder binder, object value)
+		{
+			_properties[binder.Name] = value;
+			return true;
+		}
+
+		private bool IsFunc => _func != null;
+		private bool IsAct => _act != null;
+		private bool IsInitialized => IsFunc || IsAct;
+
+		private readonly F _func;
+
+		private F Func {
+			get {
+				if (!IsFunc) throw new InvalidOperationException($"Pipe is not a function.");
+
+				return _func;
+			}
+		}
+
+		private readonly A _act;
+
+		private A Act {
+			get {
+				if (!IsAct) throw new InvalidOperationException($"Pipe is not an action.");
+
+				return _act;
+			}
+		}
+
+		public object Get()
+		{
+			if (!IsInitialized) throw new InvalidOperationException("Uninitialized pipe.");
+			if (!IsFunc) throw new InvalidOperationException("Calling wrong pipe method. Should Do() instead.");
+
+			return Func(null);
+		}
+
+		public void Do()
+		{
+			if (!IsInitialized) throw new InvalidOperationException("Uninitialized pipe.");
+			if (!IsAct) throw new InvalidOperationException("Calling wrong pipe method. Should Get() instead.");
+
+			Act(null);
+		}
+
+		/// <summary>
+		/// Returns a new pipe
+		/// </summary>
+		public static PipeBase Pipe => new PipeBase();
+
+		private PipeBase([NotNull] F func)
+		{
+			if (func == null) throw new ArgumentNullException(nameof(func));
+			_func = func;
+			_act = null;
+		}
+
+		private PipeBase([NotNull] A act)
+		{
+			if (act == null) throw new ArgumentNullException(nameof(act));
+			_act = act;
+			_func = null;
+		}
+
+		/// <summary>
+		/// Forward pipe operator
+		/// </summary>
+		public static PipeBase operator |(PipeBase lhs, PipeBase rhs)
+		{
+			if (rhs.IsFunc) {
+				Func<object, object> combined = i => rhs.Func(lhs.Func(i));
+				return new PipeBase(i => combined(i));
+			} else {
+				Action<object> combined = i => rhs.Act(lhs.Func(i));
+				return new PipeBase(i => combined(i));
+			}
+		}
+
+		/// <summary>
+		/// Forward pipe operator
+		/// </summary>
+		public static PipeBase operator |(PipeBase lhs, F rhs)
+		{
+			Func<object, object> combined = i => rhs(lhs.Func(i));
+			return new PipeBase(i => combined(i));
+		}
+
+		/// <summary>
+		/// Forward pipe operator
+		/// </summary>
+		public static PipeBase operator |(PipeBase lhs, A rhs)
+		{
+			Action<object> combined = i => rhs(lhs.Func(i));
+			return new PipeBase(i => combined(i));
+		}
+
+		/// <summary>
+		/// Forward pipe operator. Initializes a pipe with object.
+		/// </summary>
+		public static PipeBase operator |(PipeBase lhs, object rhs)
+		{
+			if (lhs.IsInitialized)
+				throw new InvalidOperationException($"Pipe is already initialized, cannot initialize with object of type {rhs.GetType()}");
+
+			return new PipeBase(i => rhs);
+		}
+
+		public static implicit operator PipeBase(F func)
+		{
+			return new PipeBase(func);
+		}
+
+		public static implicit operator PipeBase(A act)
+		{
+			return new PipeBase(act);
+		}
+	}
+	*/
+}
